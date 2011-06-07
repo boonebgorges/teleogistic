580971558872d56bc02881ebb85ae5c2a6da7508
Boone Gorges <boonebgorges@gmail.com>
2011-06-07 15:10:44 -0400

    Adds some table styling

--- page-plugins.php
+++ page-plugins.php
@@ -40,12 +40,12 @@ $cols = array(
 	),
 	array(
 		'name'		=> 'rating',
-		'title'		=> 'Average Rating',
+		'title'		=> 'Rating',
 		'default_order'	=> 'desc'
 	),
 	array(
 		'name'		=> 'num_ratings',
-		'title'		=> 'Number of Ratings',
+		'title'		=> '# Ratings',
 		'default_order' => 'desc'
 	),
 	array(
@@ -60,70 +60,71 @@ require_once( get_stylesheet_directory() . '/lib/boones-pagination.php' );
 $pagination = new BBG_CPT_Pag();
 
 get_header(); ?>
-		<div id="primary">
-			<div id="content" role="main">
-				<?php the_post(); ?>
 
-				<?php get_template_part( 'content', 'page' ); ?>
-				
-				<?php add_filter( 'bp_has_groups', 'teleogistic_sort_plugin_groups' ) ?>
-				<?php if ( bp_has_groups() ) : ?>
-					<table class="widefat">
-					
-					<thead>
-					<?php if ( $sortable->have_columns() ) : ?>
-						<?php while ( $sortable->have_columns() ) : $sortable->the_column() ?>
-						
-						<?php $sortable->the_column_th() ?>
-						
-						<?php endwhile ?>
-					<?php endif ?>
-					</thead>
-					
-					<tbody>
-					<?php while ( bp_groups() ) : bp_the_group() ?> 
-						<tr>
-							<td class="name">
-								<a href="<?php bp_group_permalink() ?>"><?php bp_group_name() ?></a>
-							</td>
-						
-							<td class="version">
-								<?php echo $groups_template->group->plugin_data['version'] ?>
-							</td>
-							
-							<td class="last_updated">
-								<?php echo $groups_template->group->plugin_data['last_updated'] ?>
-							</td>
-						
-							<td class="description">
-								<?php bp_group_description() ?>
-							</td>
-							
-							<td class="rating">
-								<?php echo $groups_template->group->plugin_data['rating'] ?>
-							</td>
-							
-							<td class="num_ratings">
-								<?php echo $groups_template->group->plugin_data['num_ratings'] ?>
-							</td>
-							
-							<td class="downloaded">
-								<?php echo number_format( $groups_template->group->plugin_data['downloaded'] ) ?>
-							</td>
-						
-						</tr>
-					<?php endwhile ?>
-					</tbody>
-					
-					</table>
-				<?php endif ?>
-				
-				<?php remove_filter( 'bp_has_groups', 'teleogistic_sort_plugin_groups' ) ?>
-				
-				<?php comments_template( '', true ); ?>
+<div id="primary">
+	<div id="content" role="main">
+		<?php the_post(); ?>
 
-			</div><!-- #content -->
-		</div><!-- #primary -->
+		<?php get_template_part( 'content', 'page' ); ?>
+		
+		<?php add_filter( 'bp_has_groups', 'teleogistic_sort_plugin_groups' ) ?>
+		<?php if ( bp_has_groups() ) : ?>
+			<table class="widefat">
+			
+			<thead>
+			<?php if ( $sortable->have_columns() ) : ?>
+				<?php while ( $sortable->have_columns() ) : $sortable->the_column() ?>
+				
+				<?php $sortable->the_column_th() ?>
+				
+				<?php endwhile ?>
+			<?php endif ?>
+			</thead>
+			
+			<tbody>
+			<?php while ( bp_groups() ) : bp_the_group() ?> 
+				<tr>
+					<td class="name">
+						<a href="<?php bp_group_permalink() ?>"><?php bp_group_name() ?></a>
+					</td>
+				
+					<td class="version">
+						<?php echo $groups_template->group->plugin_data['version'] ?>
+					</td>
+					
+					<td class="last_updated">
+						<?php echo $groups_template->group->plugin_data['last_updated'] ?>
+					</td>
+				
+					<td class="description">
+						<?php bp_group_description() ?>
+					</td>
+					
+					<td class="rating">
+						<?php echo $groups_template->group->plugin_data['rating'] ?>
+					</td>
+					
+					<td class="num_ratings">
+						<?php echo $groups_template->group->plugin_data['num_ratings'] ?>
+					</td>
+					
+					<td class="downloaded">
+						<?php echo number_format( $groups_template->group->plugin_data['downloaded'] ) ?>
+					</td>
+				
+				</tr>
+			<?php endwhile ?>
+			</tbody>
+			
+			</table>
+		<?php endif ?>
+		
+		<?php remove_filter( 'bp_has_groups', 'teleogistic_sort_plugin_groups' ) ?>
+		
+		<?php comments_template( '', true ); ?>
+
+	</div><!-- #content -->
+</div><!-- #primary -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
